7335bb884d740ecfa0c103177b5bf302f6a6267f
silva4dev <web4dev.lucas@outlook.com>
2023-04-07 19:10:11 -0300

    Alterando o GlobalStyle

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
 import { BreakpointSizes, breakAt } from "../utils/Breakpoints";
 
 const GlobalStyle = createGlobalStyle`
@@ -73,4 +75,16 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-export default GlobalStyle;
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Helmet>
+  </>
+);
+
+export default GlobalStyleComposed;
