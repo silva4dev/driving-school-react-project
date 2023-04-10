f8014cb213c37259ce223758aca65d1587f8bca0
silva4dev <web4dev.lucas@outlook.com>
2023-04-09 21:29:15 -0300

    Alterando o componente Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,25 @@
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+
+const ButtonColors = {
+  default: "default",
+  primary: "primary",
+  danger: "danger",
+};
+
+const getColorText = (props) => props.theme.colors.primary.text;
+
+const getMainColor = ({ theme, color }) => {
+  switch (color) {
+    case ButtonColors.primary:
+      return theme.colors.primary.main;
+    case ButtonColors.danger:
+      return theme.colors.danger.main;
+    default:
+      return "#e0e0e0";
+  }
+};
 
 const Button = styled.button`
   font-size: 1rem;
@@ -7,19 +27,23 @@ const Button = styled.button`
   text-transform: uppercase;
   padding: 12px 36px;
   cursor: pointer;
-  background-color: ${(props) => props.theme.colorYellow};
-  border: 2px solid ${(props) => props.theme.colorYellow};
-  color: #212121;
+  background-color: ${getMainColor};
+  border: 2px solid ${getMainColor};
+  color: ${getColorText};
 `;
+
+const ButtonWrapper = (props) => <Button {...props} />;
 
 Button.defaultProps = {
   type: "button",
   children: undefined,
+  color: "default",
 };
 
 Button.propTypes = {
   type: PropTypes.string,
   children: PropTypes.node,
+  color: PropTypes.oneOf(Object.values(ButtonColors)),
 };
 
-export default Button;
+export default ButtonWrapper;
--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Button from "./Button";
+import styled from "styled-components";
+
 export default {
   title: "Components/Button",
   component: Button,
 };
 
-export const usage = () => <Button>Meu botão</Button>;
+const ToolBar = styled.div`
+  & > * {
+    margin-right: 8px;
+  }
+`;
+
+export const usage = () => (
+  <ToolBar>
+    <Button>Default</Button>
+    <Button color="primary">Primary</Button>
+    <Button color="danger">Danger</Button>
+  </ToolBar>
+);
