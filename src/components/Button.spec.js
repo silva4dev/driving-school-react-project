8f61b35b25d40e019f5191961f440fd20472620d
silva4dev <web4dev.lucas@outlook.com>
2023-04-12 11:10:24 -0300

    Adicionando teste de trigger event on click

--- src/components/Button.spec.js
+++ src/components/Button.spec.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Button from "./Button";
-import { render, screen } from "test-utils";
+import { render, screen, fireEvent } from "test-utils";
 
 test("renders a text", () => {
   render(<Button>Click here</Button>);
   expect(screen.getByText("Click here")).toBeInTheDocument();
+});
+
+test("trigger event on click", () => {
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick} />);
+  fireEvent.click(screen.getByRole("button"));
+  expect(handleClick).toBeCalled();
 });
